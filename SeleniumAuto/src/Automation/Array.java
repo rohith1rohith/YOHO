e8caa4954edac7d29fdac5ec7922d266203192a7
modam <modam@Monisha>
2025-08-18 22:12:32 +0530

    Array commit

--- SeleniumAuto/src/Automation/Array.java
+++ SeleniumAuto/src/Automation/Array.java
@@ -1,10 +1,12 @@
 package Automation;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+ WebDriver d= new ChromeDriver();
 	}
 
 }
